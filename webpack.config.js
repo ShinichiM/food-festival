f736dc23ce54752e45360e4f289e65592af1fb22
Shinichi <miyakawashi@gmail.com>
2022-03-07 23:14:02 -0500

    webpack file-loader, image-webpack-loader, file splitting and img optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,46 @@
-const path = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        ticket: './assets/js/tickets.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        path: __dirname + '/dist',
+        filename: "[name].main.bundle.js"
     },
     plugins: [new webpack.ProvidePlugin({
         $: 'jquery',
         jQuery: 'jquery'
+    }), new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
     })],
-    mode: 'development'
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: RegExp(/\.jpg$/i),
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace('../', '/assets/')
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    }
 };
